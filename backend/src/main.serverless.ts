fcb2380b55f673432c8665493558b8a8efb8168d
FinanceScore Deploy <deploy@financescore.com>
2025-12-11 15:53:56 -0400

    Fix: Enable CORS for Vercel

--- backend/src/main.serverless.ts
+++ backend/src/main.serverless.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 export default async function (req: any, res: any) {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept, Authorization',
+    });
     app.setGlobalPrefix('api'); // Important for Vercel routing
 
     await app.init();
