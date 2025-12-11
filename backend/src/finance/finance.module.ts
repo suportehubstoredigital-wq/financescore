cdbfd16f4b77427bdde8d92db77bb2b967aab00a
FinanceScore Deploy <deploy@financescore.com>
2025-12-11 09:32:56 -0400

    Fix: Remove dependência antiga do Backend

--- backend/src/finance/finance.module.ts
+++ backend/src/finance/finance.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FinanceController } from './finance.controller';
 import { FinanceService } from './finance.service';
-import { ScoreCronService } from './score-cron.service';
+
 
 @Module({
   controllers: [FinanceController],
-  providers: [FinanceService, ScoreCronService]
+  providers: [FinanceService]
 })
 export class FinanceModule { }
