863b963adc66ac3e061fdd6f7efd3295cb48f327
suportehubstoredigital-wq <suportehubstore.digital@gmail.com>
2025-12-12 09:31:46 -0400

    Delete backend directory

--- backend/src/queues/queues.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UploadQueueService } from './upload-queue.service';
-import { UploadProcessor } from './upload-processor.service';
-
-@Module({
-  providers: [UploadQueueService, UploadProcessor],
-  exports: [UploadQueueService],
-})
-export class QueuesModule { }
